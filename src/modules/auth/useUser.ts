8aa43d580fb68955bc8f7837055e9d91a63735cf
Nanna Jesen <118171087+Missjessen@users.noreply.github.com>
2025-03-05 22:08:04 +0100

    Fix linkes

--- src/modules/auth/useUser.ts
+++ src/modules/auth/useUser.ts
@@ -16,7 +16,8 @@ export const useUser = () => {
   const fetchToken = async (email: string, password:string): Promise<void> => {
       try {
 
-        const response = await fetch(`${API_URL}/user/login`, {
+        const response = await fetch(`${API_URL.replace(/\/$/, '')}/user/login`, {
+
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -54,7 +55,9 @@ export const useUser = () => {
   const registerUser = async (name: string, email: string, password: string): Promise<void> => {
     try {
 
-      const response = await fetch(`${API_URL}/user/register`, {
+      
+        const response = await fetch(`${API_URL.replace(/\/$/, '')}/user/register`, {
+
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
